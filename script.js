504583514f170536d50c4b530f99eca7bbdfc22a
JustHuman-Git <sandro.gvexidze@gmail.com>
2026-01-19 15:58:47 +0400

    last update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 let menuIcon = document.querySelector(".menu-icon");
 let burgerbar = document.querySelector(".burgermenu");
+let container = document.querySelector(".container")
 
 menuIcon.onclick = function(){
     sidebar.classlist.toggle("small-burgermenu");
+    container.classList.toggle("large-container");
 }
 
